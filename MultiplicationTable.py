da2e754d597c7608d963a254dc4f994386997ba7
Arjun Krishna <arjun12345krishna@gmail.com>
2021-06-16 13:03:17 +0530

    Multiplication Table based on Even or Odd and upto

--- MultiplicationTable.py
+++ MultiplicationTable.py
@@ -1,9 +1,39 @@
+# x = int(input('\nEnter a Number : '))
+# print('\nMultiplication table of ' + str(x) + ' is :')
+
+# i = 1
+# while i <= 10:
+#     res = i*x
+
+#     print(str(i) + ' x ' + str(x) + ' = ' + str(res))
+#     i += 1
+
+
 x = int(input('\nEnter a Number : '))
-print('\nMultiplication table of ' + str(x) + ' is :')
 
+num = []
 i = 1
-while i <= 10:
-    res = i*x
 
-    print(str(i) + ' x ' + str(x) + ' = ' + str(res))
-    i += 1
+if x % 2 == 0:
+    print(str(x) + ' is an Even Number :')
+    while i <= x:
+        if i % 2 == 0:
+            num.append(i)
+        i += 1
+else:
+    print(str(x) + ' is an Odd Number :')
+    while i <= x:
+        if i % 2 != 0:
+            num.append(i)
+        i += 1
+
+print(num)
+
+for y in num:
+    print('\nMultiplication table of ' + str(y) + ' is :')
+    w = 1
+    while w <= 10:
+        res = y * w
+
+        print(str(y) + ' x ' + str(w) + ' = ' + str(res))
+        w += 1
