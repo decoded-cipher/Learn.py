f4013d822efbc10595dc51e8bb4a4b9f9d926aeb
Arjun Krishna <arjun12345krishna@gmail.com>
2021-06-22 03:05:01 +0530

    Arrow Pattern

--- Pattern.py
+++ Pattern.py
@@ -7,8 +7,7 @@ for i in range(0, n):
         print("* ", end="")       
     print()
 
-rows = n - 1
-for i in range(rows + 1, 0, -1):    
-    for j in range(0, i - 1):  
+for i in range(n + 1, 0, -1):    
+    for j in range(0, i):  
         print("* ", end="")  
     print()
